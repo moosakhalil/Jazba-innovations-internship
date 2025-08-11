f9e6c8f0768cdf4f2e10f928a043b58b1e47014c
Moosa Khalil <your-email@example.com>
2025-08-11 11:30:35 +0500

    Initial commit for week5 branch

---
+++ JS Var-Const-Let/intro.js
@@ -0,0 +1,28 @@
+/*practice for concepts of var,const and let
+*/
+/* var*/ 
+var name = "Moosa";
+
+var name="Tayyab"
+name="ali"
+
+var b=1
+var c='k'
+
+
+var b=62
+console.log(name,b,c);
+/*let*/
+{
+    let a=1
+
+ }
+let a=2
+console.log(a)
+
+/*const*/
+function consta(){
+    const pi=3.14
+    console.log(pi)
+}
+consta()
---
+++ Js Arrow function/arrow.js
@@ -0,0 +1,23 @@
+//syntax of arrow functions
+// if you use curly brackets must write return
+
+const addtw=(num1,num2)=>{
+    return num1+num2
+}
+console.log(addtw (3,9))
+
+
+//second method if you want to use implicit return
+
+const addt=(num1,num2)=>(num1 + num2)
+console.log(addt(5,8))
+
+
+//one parameter only
+const addtwo=(num1)=>num1+5
+console.log(addtwo(90))
+
+
+
+
+
